56ee67841bb4f8d4e35ca8851fd4fc69597b663f
Thomas Kemmer <tkemmer@computer.org>
2014-01-29 19:29:21 +0100

    usability improvements

--- mopidy_internetarchive/playback.py
+++ mopidy_internetarchive/playback.py
@@ -1,12 +1,18 @@
 from __future__ import unicode_literals
 
+import logging
+
 from mopidy import backend
+from uritools import urisplit
+
+logger = logging.getLogger(__name__)
 
 
 class InternetArchivePlaybackProvider(backend.PlaybackProvider):
 
     def play(self, track):
-        u = self.backend.parse_uri(track.uri)
-        uri = self.backend.client.geturl(u['path'], u['fragment'])
+        uriparts = urisplit(track.uri)
+        uri = self.backend.client.geturl(uriparts.path, uriparts.fragment)
+        logger.debug("internetarchive playback uri: %s", uri)
         track = track.copy(uri=uri)
         return super(InternetArchivePlaybackProvider, self).play(track)
