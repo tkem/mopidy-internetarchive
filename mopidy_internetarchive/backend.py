904a8cf5c57aa61a5d1b0c0e5d6fdf47795bd1c3
Thomas Kemmer <tkemmer@computer.org>
2014-11-18 15:52:48 +0100

    Refactor async bookmarks loading

--- mopidy_internetarchive/backend.py
+++ mopidy_internetarchive/backend.py
@@ -9,7 +9,6 @@ from . import Extension
 from .client import InternetArchiveClient
 from .library import InternetArchiveLibraryProvider
 from .playback import InternetArchivePlaybackProvider
-from .bookmarks import InternetArchiveBookmarksActor
 from .playlists import InternetArchivePlaylistsProvider
 
 logger = logging.getLogger(__name__)
@@ -21,29 +20,16 @@ class InternetArchiveBackend(pykka.ThreadingActor, backend.Backend):
 
     def __init__(self, config, audio):
         super(InternetArchiveBackend, self).__init__()
-
-        ext_config = config[Extension.ext_name]
-        base_url = ext_config['base_url']
-        username = ext_config['username']
-        timeout = ext_config['timeout']
-
-        self.client = InternetArchiveClient(base_url, timeout)
+        self.client = InternetArchiveClient(
+            config[Extension.ext_name]['base_url'],
+            config[Extension.ext_name]['timeout']
+        )
         self.library = InternetArchiveLibraryProvider(config, self)
         self.playback = InternetArchivePlaybackProvider(audio, self)
-
-        if username:
-            self.bookmarks = InternetArchiveBookmarksActor.start(
-                username, backend=self
-            ).proxy()
-            self.playlists = InternetArchivePlaylistsProvider(self)
-        else:
-            self.bookmarks = None
-            self.playlists = None
+        self.playlists = InternetArchivePlaylistsProvider(config, self)
 
     def on_start(self):
-        if self.bookmarks:
-            self.bookmarks.refresh()
+        self.playlists.start()
 
     def on_stop(self):
-        if self.bookmarks:
-            self.bookmarks.stop()
+        self.playlists.stop()
--- mopidy_internetarchive/playlists.py
+++ mopidy_internetarchive/playlists.py
@@ -1,9 +1,65 @@
 from __future__ import unicode_literals
 
+import logging
+import pykka
+
 from mopidy import backend
+from mopidy.models import Playlist
+
+from . import Extension
+
+logger = logging.getLogger(__name__)
+
+
+class InternetArchiveBookmarks(pykka.ThreadingActor):
+
+    pykka_traversable = True
+
+    def __init__(self, username, backend):
+        super(InternetArchiveBookmarks, self).__init__()
+        self.username = username
+        # always access backend by proxy
+        self.backend = backend.actor_ref.proxy()
+        # for delaying work to our future self
+        self.future = self.actor_ref.proxy()
+
+    def refresh(self):
+        try:
+            bookmarks = self.backend.client.bookmarks(self.username).get()
+            logger.debug('Received %d Archive bookmarks', len(bookmarks))
+            self.future.load(bookmarks)
+        except Exception as e:
+            logger.error('Error loading Archive bookmarks: %s', e)
+
+    def load(self, bookmarks, playlists=[]):
+        playlists = playlists[:]
+        if bookmarks:
+            doc = bookmarks.pop(0)
+            id = doc['identifier']
+            uri = '%s:%s' % (Extension.ext_name, id)
+            name = doc.get('title', id)
+            tracks = self.backend.library.lookup(uri).get()
+            if tracks:
+                playlists.append(Playlist(uri=uri, name=name, tracks=tracks))
+            else:
+                logger.warn('Skipping Archive bookmark %s', name)
+            self.future.load(bookmarks, playlists)
+        else:
+            logger.info('Loaded %d Archive bookmarks', len(playlists))
+            self.backend.playlists.playlists = playlists
+            backend.BackendListener.send('playlists_loaded')
 
 
 class InternetArchivePlaylistsProvider(backend.PlaylistsProvider):
+
+    def __init__(self, config, backend):
+        super(InternetArchivePlaylistsProvider, self).__init__(backend)
+        username = config[Extension.ext_name]['username']
+        if username:
+            self.bookmarks = InternetArchiveBookmarks.start(username, backend)
+        else:
+            self.bookmarks = None
+        self.timeout = config[Extension.ext_name]['timeout']
 
     def create(self, name):
         pass  # TODO
@@ -18,7 +74,17 @@ class InternetArchivePlaylistsProvider(backend.PlaylistsProvider):
         return None
 
     def refresh(self):
-        self.backend.bookmarks.refresh()
+        if self.bookmarks:
+            self.bookmarks.proxy().refresh()
 
     def save(self, playlist):
         pass  # TODO
+
+    def start(self):
+        if self.bookmarks:
+            self.bookmarks.proxy().refresh()
+
+    def stop(self):
+        if self.bookmarks:
+            logger.debug('Stopping %s', self.bookmarks)
+            self.bookmarks.stop(block=False)  # FIXME
