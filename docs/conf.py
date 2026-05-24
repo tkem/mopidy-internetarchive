bcfa3c37405fd0076cb825dde2c1dca65dd586a1
Thomas Kemmer <tkemmer@computer.org>
2026-05-25 00:05:04 +0200

    Documentation fixup.

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,23 @@
-import importlib.metadata
+# Extract the current version from the source.
+import pathlib
+import sys
+
+src_directory = (pathlib.Path(__file__).parent.parent / "src").resolve()
+sys.path.insert(0, str(src_directory))
+
+
+def get_version(package):
+    """Get the version and release from the source code."""
+    text = (src_directory / "mopidy_internetarchive/__init__.py").read_text()
+    for line in text.splitlines():
+        if not line.strip().startswith("__version__"):
+            continue
+        return line.partition("=")[2].strip().strip("\"'")
 
 
 project = "Mopidy-InternetArchive"
 copyright = "2014-2026 Thomas Kemmer"
-release = importlib.metadata.version(project)
+release = get_version(project)
 version = ".".join(release.split(".")[:2])
 
 extensions = [
